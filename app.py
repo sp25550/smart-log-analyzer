b794215855db691677b846291a8df92a8353cb1c
Shreya <netajishreya@gmail.com>
2026-04-19 01:34:02 +0530

    App.pyu update

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ def index():
 
 if __name__ == "__main__":
     # IMPORTANT for Jenkins + Selenium stability
-    app.run(host="127.0.0.1", port=5000, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
